232c237b39a056daeb3dd0ced0e49617eb4e4f88
Rakka Alhazimi <rakkakeren@gmail.com>
2021-01-25 13:34:46 +0700

    add text_sum, css var and fix navbar

---
+++ js/text_sum.js
@@ -0,0 +1,29 @@
+var summary;
+article = document.getElementById("article");
+result = document.getElementById("result");
+block = document.querySelector(".app__result");
+
+
+function request_summary(req_data) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+	   summary = this.responseText;
+	   result.value = summary;
+	}
+  }
+
+  xhttp.open("POST", "http://127.0.0.1:5000/text_sum/", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(req_data);
+}
+
+function get_summary() {
+
+  data = {"date": Date.now(), "article": article.value}
+  data = JSON.stringify(data)
+
+  request_summary(data);
+  block.style.display = "flex";
+  window.location.href = "#result";
+ }
