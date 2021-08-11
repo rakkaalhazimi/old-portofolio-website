4956147f6a8c467ca627662385f7d9da004a74ad
Rakka Alhazimi <rakkakeren@gmail.com>
2021-08-11 16:53:21 +0700

    js for multiple choices sheet

--- js/questions.js
+++ js/questions.js
@@ -1,31 +1,22 @@
 let counts = document.getElementById("counts");
-let gen_btn = document.getElementById("gen-btn");
-let sheet = document.getElementById("sheet");
 
-function inputErrorMsg() {
-	alert("Please input any number from 1 - 200");
+function inputError() {
+	alert("Please insert a number between 1 - 200");
+}
+
+function generate(event) {
+	if (Number(counts.value)) {
+		console.log("success");
+	} else {
+		inputError();
+	};
 }
 
 function elt(type, ...children) {
-	let node = document.createElement(type);
-	for (let child of children) {
-		if (typeof child != "string") node.appendChild(child);
-		else node.appendChild(document.createTextNode(child));
-	}
-	return node;
-}
-
-function generate() {
-	let number = Number(counts.value);
-	if (number) {
-		
-		counts.value = "";
-		sheet.appendChild(elt("p", "rakka"));
-		
-	} else {
-		inputErrorMsg();
-	}
-	
-}
-
-//gen_btn.addEventListener("click", generate);
+      let node = document.createElement(type);
+      for (let child of children) {
+        if (typeof child != "string") node.appendChild(child);
+        else node.appendChild(document.createTextNode(child));
+      }
+      return node;
+    }
