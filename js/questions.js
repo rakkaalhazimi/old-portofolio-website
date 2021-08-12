753189398be00fd20e64adf2c2c67cb5f97c1577
Rakka Alhazimi <rakkakeren@gmail.com>
2021-08-12 13:23:39 +0700

    readjust generate button

--- js/questions.js
+++ js/questions.js
@@ -1,42 +1,77 @@
 // global variables
 let counts = document.getElementById("counts");
 let sheet = document.getElementById("sheet");
+let genBtn = document.getElementById("gen-btn");
+let modalYesBtn = document.getElementById("modal-yes-btn");
+let myModal = new bootstrap.Modal(document.getElementById("exampleModal"))
+
 const limit = 10;
+
+// EventListener
+genBtn.addEventListener("click", generate);
 
 // Error Message
 function inputError() {
 	alert("Please insert a number between 1 - 200");
 }
 
+// Continue or not button for modal
+function continuePress() {
+	continueState = true
+}
+
+function cancelPress() {
+	continueState = false
+}
+
 // HTML Tags Creator
 function elt(type, cls="", attr="", ...children) {
-      let node = document.createElement(type);
-			// add class name
-			node.className = cls;
-			
-			// add attributes
-			if (attr) {
-				console.log(typeof attr);
-				for (let key in attr) {
-					node.setAttribute(key, attr[key]);
-				}
-			}
-			
-			// add children
-      for (let child of children) {
-				console.log(child);
-        if (typeof child != "string") node.appendChild(child);
-        else node.appendChild(document.createTextNode(child));
-      }
-      return node;
-    }
+	let node = document.createElement(type);
+	// add class name
+	node.className = cls;
+	
+	// add attributes
+	if (attr) {
+		console.log(typeof attr);
+		for (let key in attr) {
+			node.setAttribute(key, attr[key]);
+		}
+	}
+	
+	// add children
+	for (let child of children) {
+		console.log(child);
+		if (typeof child != "string") node.appendChild(child);
+		else node.appendChild(document.createTextNode(child));
+	}
+	return node;
+}
 
-// Toogle Sheet Creation
+
+// Function to remove all result content
+function remove_child(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+// Toggle Sheet Creation
 function generate(event) {
-	let counts_value = Number(counts.value);
-	if (counts_value) {
-		console.log("success");
-		let node = createOuterOptionsTag(counts_value)
+	let countsValue = Number(counts.value);
+	if (countsValue) {
+		
+		// First time sheet will skip, otherwise warn the user
+		if (sheet.childElementCount) {
+			myModal.show();
+			
+			// Delete previous sheet children
+			remove_child(sheet);
+		}
+			
+		
+		
+		// Then add the nodes
+		let node = createOuterOptionsTag(countsValue)
 		sheet.appendChild(node);
 		
 		
@@ -59,17 +94,17 @@ function createOptionsTag(num) {
 	
 	// ABCDE radio button
 	for (opt of "ABCDE") {
-		let input_node = elt("input", 
+		let inputNode = elt("input", 
 												 cls="form-check-input",
 												 attr={"type": "radio", 
 															 "name": `number-${num}`,
 															 "id": `options-${num}-${opt}`})
-		let label_node = elt("label",
+		let labelNode = elt("label",
 												 cls="form-check-label",
 												 attr={"for": `options-${num}-${opt}`},
 												 children=`${opt}`)
 		
-		let radio_nodes = [input_node, label_node];
+		let radio_nodes = [inputNode, labelNode];
 		// Wrap all options inside this tag
 		let form_check = elt("div", cls="form-check", attr="", ...radio_nodes);										 
 											 
@@ -96,11 +131,11 @@ function createInnerOptionsTag(start, end) {
 
 function createMiddleOptionsTag(sheet_number) {
 	// Inner Elements Tags
-	let upper_bound = sheet_number * limit; // upper limit is 10 or multiple
-	let lower_bound = upper_bound - (limit - 1); // lower limit is 
+	let upperBound = sheet_number * limit; // upper limit is 10 or multiple
+	let lowerBound = upperBound - (limit - 1); // lower limit is 
 	
-	let innerElements = createInnerOptionsTag(start=lower_bound, 
-																						end=upper_bound);
+	let innerElements = createInnerOptionsTag(start=lowerBound, 
+																						end=upperBound);
 	
 	// Div tag to wrap inner elements
 	let middleNearWrapper = elt("div", "row row-cols-1", "", ...innerElements);
@@ -110,11 +145,11 @@ function createMiddleOptionsTag(sheet_number) {
 }
 
 
-function createOuterOptionsTag(counts_value) {
+function createOuterOptionsTag(countsValue) {
 	// List of tags needed
 	let nodes = [];
 	
-	let numberOfSheets = Math.ceil(counts_value / limit);
+	let numberOfSheets = Math.ceil(countsValue / limit);
 	console.log("Debug " + numberOfSheets);
 	for (let num=1; num <= numberOfSheets; num++) {
 		// Middle Elements Tags
